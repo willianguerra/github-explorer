50b0bdf557076ef8b259756fe99dcab1d0963982
willianGuerra <willianguerrapossamai0901@gmail.com>
2021-11-08 19:44:49 -0300

    first-commit

---
+++ src/App.tsx
@@ -0,0 +1,6 @@
+import { RepositoryList } from './components/RepositoryList'
+import './styles/globals.scss'
+
+export function App() {
+    return <RepositoryList />
+}
---
+++ src/components/RepositoryList.tsx
@@ -0,0 +1,33 @@
+import { RepositoryItem } from "./RepositoryItem";
+import '../styles/repostitories.scss';
+import { useEffect, useState } from "react";
+
+interface Repository {
+  name: string;
+  description: string;
+  html_url: string;
+}
+
+export function RepositoryList() {
+
+    const [ repositories, setRepositories ] = useState<Repository[]>([]);
+
+    useEffect(() =>{
+        fetch('https://api.github.com/users/willianguerra/repos')
+        .then(response => response.json())
+        .then(data => setRepositories(data))
+    }, []);
+
+
+  return (
+    <section className="repository-list">
+      <h1>Lista de repositorios</h1>
+
+      <ul>
+          {repositories.map(repository => (
+              <RepositoryItem key={repository.name} repository={repository}/>
+          ))}
+      </ul>
+    </section>
+  );
+}
